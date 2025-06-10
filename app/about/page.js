076afcf9127c3078ebf69f2372a205b749fafa64
sharma jayesh ruplal <jayesh.200410116043@gmail.com>
2025-06-10 13:41:31 +0530

    this is my third psuh about page created

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+// app/about/page.js
+
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">About Us</h1>
+    </div>
+  );
+}
